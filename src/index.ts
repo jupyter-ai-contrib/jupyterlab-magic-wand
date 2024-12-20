8e7625fc42820b94946d59236ae2bde6b5a11c2c
Zach Sailer <zsailer@apple.com>
2024-12-20 14:07:00 -0800

    Add image

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ import {
   // responseHandledData
 } from './celltracker';
 
-// import {
-//   findCell,
-// } from './utils';
+import {
+  findCell,
+} from './utils';
 
 // import { executeFeedbackCommand } from './feedback';
 import { IEventListener } from 'jupyterlab-eventlistener';
@@ -21,121 +21,121 @@ import { ICellFooterTracker } from 'jupyterlab-cell-input-footer';
 
 const PLUGIN_ID = 'jupyterlab_magic_wand';
 
-// const agentCommands: JupyterFrontEndPlugin<void> = {
-//   id: PLUGIN_ID + ":agentCommands",
-//   description: 'A set of custom commands that AI agents can use.',
-//   autoStart: true,
-//   requires: [INotebookTracker],
-//   activate: async (
-//     app: JupyterFrontEnd,
-//     notebookTracker: INotebookTracker,
-//   ) => {
-//     console.log(`Jupyter Magic Wand plugin extension activated: ${PLUGIN_ID}:agentCommands`);
-//     app.commands.addCommand(
-//       'insert-cell-below',
-//       {
-//         execute: (args) => {
-//           let data = (args as any);
-//           let cellId = data["cell_id"];
-//           let newCellId = data["new_cell_id"] || undefined;
-//           let cellType = data["cell_type"]
-//           if (cellId) {
-//             let { notebook } = findCell(cellId, notebookTracker);
-//             let idx = notebook?.model?.sharedModel.cells.findIndex((cell) => {
-//               return cell.getId() == cellId
-//             })
-//             if (idx !== undefined && idx >= 0) {
-//               let newCell = notebook?.model?.sharedModel.insertCell(
-//                 idx + 1, {
-//                   cell_type: cellType,
-//                   metadata: {},
-//                   id: newCellId
-//                 })
-//               if (data["source"]) {
-//                 // Add the source to the new cell;
-//                 newCell?.setSource(data["source"]);
-//                 // Post an update to ensure that notebook gets rerendered.
-//                 notebook?.update();
-//               }
-//             }
-//           }
-//         }
-//       }
-//     )
-//     app.commands.addCommand(
-//       'update-cell-source',
-//       {
-//         execute: (args) => {
-//           let data = (args as any);
-//           let cellId = data["cell_id"];
-//           if (cellId) {
-//             let { notebook } = findCell(cellId, notebookTracker);
-//             let cell = notebook?.model?.sharedModel.cells.find((cell) => {
-//               return cell.getId() == cellId
-//             })
-//             if (cell) {
-//               if (data["source"]) {
-//                 // Add the source to the new cell;
-//                 cell?.setSource(data["source"]);
-//                 // Post an update to ensure that notebook gets rerendered.
-//                 notebook?.update();
-//                 notebook?.content.update();
-//               }
-//             }
-//           }
-//         }
-//       }
-//     )
-//     app.commands.addCommand(
-//       'track-if-editted',
-//       {
-//         execute: async (args) => {
-//           let data = (args as any);
-//           let cellId = data["cell_id"];
-//           // don't do anything if no cell_id was given.
-//           if (!cellId) {
-//             return;
-//           }
+const agentCommands: JupyterFrontEndPlugin<void> = {
+  id: PLUGIN_ID + ":agentCommands",
+  description: 'A set of custom commands that AI agents can use.',
+  autoStart: true,
+  requires: [INotebookTracker],
+  activate: async (
+    app: JupyterFrontEnd,
+    notebookTracker: INotebookTracker,
+  ) => {
+    console.log(`Jupyter Magic Wand plugin extension activated: ${PLUGIN_ID}:agentCommands`);
+    app.commands.addCommand(
+      'insert-cell-below',
+      {
+        execute: (args) => {
+          let data = (args as any);
+          let cellId = data["cell_id"];
+          let newCellId = data["new_cell_id"] || undefined;
+          let cellType = data["cell_type"]
+          if (cellId) {
+            let { notebook } = findCell(cellId, notebookTracker);
+            let idx = notebook?.model?.sharedModel.cells.findIndex((cell) => {
+              return cell.getId() == cellId
+            })
+            if (idx !== undefined && idx >= 0) {
+              let newCell = notebook?.model?.sharedModel.insertCell(
+                idx + 1, {
+                  cell_type: cellType,
+                  metadata: {},
+                  id: newCellId
+                })
+              if (data["source"]) {
+                // Add the source to the new cell;
+                newCell?.setSource(data["source"]);
+                // Post an update to ensure that notebook gets rerendered.
+                notebook?.update();
+              }
+            }
+          }
+        }
+      }
+    )
+    app.commands.addCommand(
+      'update-cell-source',
+      {
+        execute: (args) => {
+          let data = (args as any);
+          let cellId = data["cell_id"];
+          if (cellId) {
+            let { notebook } = findCell(cellId, notebookTracker);
+            let cell = notebook?.model?.sharedModel.cells.find((cell) => {
+              return cell.getId() == cellId
+            })
+            if (cell) {
+              if (data["source"]) {
+                // Add the source to the new cell;
+                cell?.setSource(data["source"]);
+                // Post an update to ensure that notebook gets rerendered.
+                notebook?.update();
+                notebook?.content.update();
+              }
+            }
+          }
+        }
+      }
+    )
+    app.commands.addCommand(
+      'track-if-editted',
+      {
+        execute: async (args) => {
+          let data = (args as any);
+          let cellId = data["cell_id"];
+          // don't do anything if no cell_id was given.
+          if (!cellId) {
+            return;
+          }
 
-//           let { cell, notebook } = findCell(cellId, notebookTracker);
-//           if (cell === undefined) {
-//             return;
-//           }
-//           await cell.ready;
+          let { cell, notebook } = findCell(cellId, notebookTracker);
+          if (cell === undefined) {
+            return;
+          }
+          await cell.ready;
 
-//           let sharedCell = notebook?.model?.sharedModel.cells.find((cell) => {
-//             return cell.getId() == cellId
-//           })
-//           if (sharedCell === undefined) {
-//             return;
-//           }
+          let sharedCell = notebook?.model?.sharedModel.cells.find((cell) => {
+            return cell.getId() == cellId
+          })
+          if (sharedCell === undefined) {
+            return;
+          }
 
-//           function updateMetadata(editted: boolean = false) {
-//             let metadata: object = {}
-//             try {
-//               metadata = cell?.model.getMetadata("jupyter_ai") || {}
-//             } catch {
-//               metadata = {}
-//             }
-//             let newMetadata = {
-//               ...metadata,
-//               editted: editted
-//             }
-//             // cell?.model.sharedModel.me
-//             cell?.model.setMetadata("jupyter_ai", newMetadata);
-//           }
-//           updateMetadata(false);
-//           let updateAIEditedField = function() {
-//             updateMetadata(true);
-//             sharedCell?.changed.disconnect(updateAIEditedField);
-//           }
-//           sharedCell?.changed.connect(updateAIEditedField);
-//         }
-//       }
-//     )
+          function updateMetadata(editted: boolean = false) {
+            let metadata: object = {}
+            try {
+              metadata = cell?.model.getMetadata("jupyter_ai") || {}
+            } catch {
+              metadata = {}
+            }
+            let newMetadata = {
+              ...metadata,
+              editted: editted
+            }
+            // cell?.model.sharedModel.me
+            cell?.model.setMetadata("jupyter_ai", newMetadata);
+          }
+          updateMetadata(false);
+          let updateAIEditedField = function() {
+            updateMetadata(true);
+            sharedCell?.changed.disconnect(updateAIEditedField);
+          }
+          sharedCell?.changed.connect(updateAIEditedField);
+        }
+      }
+    )
 
-//   }
-// }
+  }
+}
 
 /**
  * Initialization data for the jupyterlab-magic-wand extension.
@@ -211,4 +211,4 @@ const plugin: JupyterFrontEndPlugin<IAICellTracker> = {
 //   }
 // };
 
-export default [plugin];
+export default [plugin, agentCommands];
