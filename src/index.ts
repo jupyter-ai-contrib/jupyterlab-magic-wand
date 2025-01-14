52df7d3639a0aa580fc3e69040b340a4bbefcf0d
Zach Sailer <zsailer@apple.com>
2025-01-13 17:01:13 -0800

    refactor to make pending button separate

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,53 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import { Widget } from '@lumino/widgets';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { INotebookTracker } from '@jupyterlab/notebook';
-import {
-  AICellTracker,
-  IAICellTracker
-  // responseHandledData
-} from './celltracker';
-
-import { findCell } from './utils';
-
-// import { executeFeedbackCommand } from './feedback';
+import { Event } from '@jupyterlab/services';
+import { errorDialog } from './components/errordialog';
+import { Notification } from '@jupyterlab/apputils';
+import { 
+  getCurrentActiveCell,
+  getActiveCellContext,
+  findCell
+} from './utils';
+import { requestAPI } from './handler';
 import { IEventListener } from 'jupyterlab-eventlistener';
 import { ICellFooterTracker } from 'jupyterlab-cell-input-footer';
+import { PendingCellCommand } from './pendingCellCommand';
+import { wandIcon } from './icon';
+
 
 const PLUGIN_ID = 'jupyterlab_magic_wand';
+const AI_EVENT_SCHEMA_ID =
+  'https://events.jupyter.org/jupyter_ai/magic_button/v1';
+const AI_ERROR_EVENT_SCHEMA_ID =
+  'https://events.jupyter.org/jupyter_ai/error/v1';
+
+export type ERROR_EVENT = {
+  type: string;
+  id: string;
+  time: string;
+  reply_to: string;
+  error_type: string;
+  message: string;
+};
+
+
+export type LabCommand = {
+  name: string;
+  args: any;
+};
+
+export type AIWorkflowState = {
+  agent: string;
+  input: string;
+  context: any;
+  messages?: Array<string>;
+  commands: Array<LabCommand>;
+};
+
 
 const agentCommands: JupyterFrontEndPlugin<void> = {
   id: PLUGIN_ID + ':agentCommands',
@@ -126,13 +157,12 @@ const agentCommands: JupyterFrontEndPlugin<void> = {
 /**
  * Initialization data for the jupyterlab-magic-wand extension.
  */
-const plugin: JupyterFrontEndPlugin<IAICellTracker> = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: PLUGIN_ID + ':plugin',
   description: 'A cell tracker for the magic wand button.',
   autoStart: true,
   optional: [ISettingRegistry],
   requires: [INotebookTracker, IEventListener, ICellFooterTracker],
-  provides: IAICellTracker,
   activate: async (
     app: JupyterFrontEnd,
     notebookTracker: INotebookTracker,
@@ -140,16 +170,124 @@ const plugin: JupyterFrontEndPlugin<IAICellTracker> = {
     cellFooterTracker: ICellFooterTracker
   ) => {
     console.log(
-      `Jupyter Magic Wand plugin extension activated: ${PLUGIN_ID}:tracker`
+      `Jupyter Magic Wand plugin extension activated: ${PLUGIN_ID}:button`
     );
-    const aiCellTracker = new AICellTracker(
-      app.commands,
+    await app.serviceManager.ready;
+
+    const command = new PendingCellCommand(
       notebookTracker,
-      eventListener,
-      cellFooterTracker
+      app.commands,
+      {
+        id: 'jupyterlab_magic_wand:execute',
+        label: 'AI, please help!',
+        pendingLabel: 'AI is thinking...',
+        icon: wandIcon,
+        timeout: 10000,
+        execute: () => {
+          const currentNotebook = notebookTracker.currentWidget;
+          const cellContext = getActiveCellContext(currentNotebook);
+          const cell = getCurrentActiveCell(notebookTracker);
+
+          if (!cellContext) {
+            console.log('AI Command not focused on a cell.');
+            return;
+          }
+
+          const cellId = cellContext.current.cell_id;
+          const codeInput = cell?.model?.sharedModel.getSource();
+
+          // Make the request.
+          requestAPI('/api/ai/magic', {
+            method: 'POST',
+            body: JSON.stringify({
+              input: codeInput,
+              context: {
+                cell_id: cellId,
+                content: currentNotebook?.content.model?.toJSON()
+              },
+              commands: []
+            })
+          });
+        },
+        complete: (args) => {
+          const data = (args as AIWorkflowState);
+      
+          const cellId = data.context['cell_id'];
+      
+          if (cellId) {
+            const { cell } = findCell(cellId, notebookTracker);
+            cell?.model.setMetadata('editable', true);
+            cell?.saveEditableState();
+            // Remove pending item from cell map
+            const metadata = cell?.model.getMetadata('jupyter_ai');
+            const newMetadata = {
+              ...metadata,
+              agent: data.agent,
+              messages: data.messages
+            };
+            cell?.model.setMetadata('jupyter_ai', newMetadata);
+            if (cell) {
+              const footer = cellFooterTracker.getFooter(cellId);
+              // Add a magic icon to the cell toolbar.
+              // (remove old ones too).
+              footer?.removeToolbarItem('magicIcon');
+              const iconWidget = new Widget({ node: wandIcon.element() });
+              iconWidget.addClass('jp-Toolbar-Icon');
+              footer?.addToolbarItemOnLeft('magicIcon', iconWidget);
+              cellFooterTracker.showFooter(cellId);
+            }
+          }
+      
+          if (data.commands) {
+            data.commands.forEach(async (command: LabCommand) => {
+              try {
+                await app.commands.execute(command.name, command.args);
+              } catch (err) {
+                console.log('Could not execute AI command: ' + command.name);
+                console.error(err);
+              }
+            });
+          }
+        },
+        error: () => {}
+      }
     );
-    return aiCellTracker;
+
+    eventListener.addListener(
+      AI_EVENT_SCHEMA_ID,
+      async (manager, schemaId, event: Event.Emission) => {
+        let args = (event as any as AIWorkflowState);
+        await command.complete(args.context.cell_id, args);
+      }
+    );
+
+    eventListener.addListener(
+      AI_ERROR_EVENT_SCHEMA_ID,
+      async (manager, schemaId, event: Event.Emission) => {
+        const data = event as any as ERROR_EVENT;
+
+        const { cell } = findCell(data.reply_to, notebookTracker);
+        cell?.model.setMetadata('editable', true);
+        cell?.saveEditableState();
+
+        // Raise a notification to the user
+        Notification.error('An error occurred with the AI Magic button.', {
+          autoClose: 5000,
+          actions: [
+            {
+              label: 'Read more',
+              callback: () => {
+                errorDialog(data.error_type, data.message);
+              }
+            }
+          ]
+        });
+      }
+    );
   }
 };
+
+
+
 
 export default [plugin, agentCommands];
--- src/utils.ts
+++ src/utils.ts
@@ -44,6 +44,11 @@ export type CellContext = {
   next: CellData | null;
 };
 
+type ActiveNotebookCell = {
+  cell: Cell | undefined;
+  notebook: NotebookPanel | undefined;
+};
+
 export function getCellData(cell: Cell): CellData {
   const source = cell?.model.sharedModel.getSource();
   cell?.model.selections;
@@ -112,36 +117,64 @@ export function getActiveCellContext(
   };
 }
 
-type ActiveNotebookCell = {
-  cell: Cell | undefined;
-  notebook: NotebookPanel | undefined;
-};
+/**
+ * Simple method to get the active cell.
+ *
+ * @returns
+ */
+export function getCurrentActiveCell(notebookTracker: INotebookTracker): Cell | null | undefined {
+  return notebookTracker.currentWidget?.content.activeCell;
+}
 
-export function findCell(
-  cellId: string,
-  notebookTracker: INotebookTracker
-): ActiveNotebookCell {
-  // First, try the current notebook in focuse
-  const currentNotebook = notebookTracker.currentWidget;
-  const cell =
-    notebookTracker.currentWidget?.content._findCellById(cellId)?.cell;
-  if (currentNotebook && cell) {
+/**
+ * Get the current active cell's ID.
+ *
+ * @returns
+ */
+export function getCurrentActiveCellId(notebookTracker: INotebookTracker): string | null | undefined {
+  const notebook = notebookTracker.currentWidget;
+  const idx = notebook?.content.activeCellIndex;
+  if (idx !== undefined && notebook) {
+    return notebook.model?.cells.get(idx)?.id;
+  }
+}
+
+  /**
+   * Find a cell based on it's unique ID.
+   *
+   * This will start by searching the currently
+   * active notebook. If the ID is not present there,
+   * we will iterate through all notebooks until
+   * we find the cell. If the cell is never found,
+   * return undefined.
+   *
+   * @param cellId
+   * @returns
+   */
+  export function findCell(
+    cellId: string,
+    notebookTracker: INotebookTracker
+  ): ActiveNotebookCell {
+    // First, try the current notebook in focuse
+    const currentNotebook = notebookTracker.currentWidget;
+    const cell =
+      notebookTracker.currentWidget?.content._findCellById(cellId)?.cell;
+    if (currentNotebook && cell) {
+      return {
+        cell: cell,
+        notebook: currentNotebook
+      };
+    }
+    // Otherwise iterate through notebooks to find the cell.
+    const notebookMatch = notebookTracker.find(notebook => {
+      const cell = notebook.content._findCellById(cellId)?.cell;
+      if (cell) {
+        return true;
+      }
+      return false;
+    });
     return {
       cell: cell,
-      notebook: currentNotebook
+      notebook: notebookMatch
     };
   }
-
-  // Otherwise iterate through notebooks to find the cell.
-  const notebookMatch = notebookTracker.find(notebook => {
-    const cell = notebook.content._findCellById(cellId)?.cell;
-    if (cell) {
-      return true;
-    }
-    return false;
-  });
-  return {
-    cell: cell,
-    notebook: notebookMatch
-  };
-}
